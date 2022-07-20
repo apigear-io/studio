8f8e89d8309d2d26c3c55e1609537200d13575d2
Jürgen Ryannel <juergen@ryannel.org>
2022-07-20 09:26:06 +0200

    ts ipc

--- frontend/src/wailsjs/go/models.ts
+++ frontend/src/wailsjs/go/models.ts
@@ -1,21 +1,5 @@
 export namespace main {
 	
-	export class AppSettings {
-	    server_port: number;
-	    update_channel: string;
-	    editor_command: string;
-	
-	    static createFrom(source: any = {}) {
-	        return new AppSettings(source);
-	    }
-	
-	    constructor(source: any = {}) {
-	        if ('string' === typeof source) source = JSON.parse(source);
-	        this.server_port = source["server_port"];
-	        this.update_channel = source["update_channel"];
-	        this.editor_command = source["editor_command"];
-	    }
-	}
 	export class DocumentInfo {
 	    name: string;
 	    path: string;
@@ -86,6 +70,22 @@ export namespace main {
 	        this.path = source["path"];
 	        this.installed = source["installed"];
 	        this.available = source["available"];
+	    }
+	}
+	export class AppSettings {
+	    server_port: number;
+	    update_channel: string;
+	    editor_command: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new AppSettings(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.server_port = source["server_port"];
+	        this.update_channel = source["update_channel"];
+	        this.editor_command = source["editor_command"];
 	    }
 	}
 
