303db903299b9fae9aefcd7025e8513ceed46842
Jürgen Ryannel <juergen.ryannel@icloud.com>
2022-12-08 16:09:41 +0100

    sim events upgrade (#14)
    * wip sim events
    
    * update simulation events
    
    * upgrade cli to v0.19.0

--- frontend/src/pages/SimulationMessagesPage.vue
+++ frontend/src/pages/SimulationMessagesPage.vue
@@ -3,7 +3,7 @@
     <q-card>
       <q-card-section>
         <q-toolbar class="bg-primary text-white">
-          <q-avatar icon="av_timer"/>
+          <q-avatar icon="av_timer" />
           <q-toolbar-title>Simulation Events</q-toolbar-title>
           <q-space />
           <q-toggle size="sm" v-model="state.isListening" label="Scroll" style="min-width: 120px" />
@@ -11,7 +11,7 @@
         </q-toolbar>
       </q-card-section>
       <q-card-section>
-        <q-table table-class="text-positive" table-header-class="text-primary" :rows="simu.events" :columns="columns" row-key="id" dense flat :pagination="state.initialPagination" />
+        <q-table table-class="text-positive" table-header-class="text-primary" :rows="simu.events" :columns="columns" row-key="timestamp" dense flat :pagination="state.initialPagination" />
       </q-card-section>
     </q-card>
   </q-page>
@@ -24,10 +24,12 @@ import { useSimulationStore } from '../stores/simulation-store';
 
 const columns: QTableProps['columns'] = [
   { name: 'time', label: 'Time', field: 'timestamp', align: 'left', style: 'width: 120px', sortable: true },
-  { name: 'source', label: 'Source', field: 'source', align: 'center', style: 'width: 80px' },
-  { name: 'type', label: 'Type', field: 'type', align: 'center', style: 'width: 80px' },
-  { name: 'symbol', label: 'Symbol', field: 'symbol', align: 'left', style: 'width: 240px' },
-  { name: 'data', label: 'Data', field: 'data', align: 'center', style: 'width: 480px' },
+  { name: 'type', label: 'Type', field: 'type', align: 'left', style: 'width: 80px' },
+  { name: 'symbol', label: 'Symbol', field: 'symbol', align: 'left', style: 'width: 80px' },
+  { name: 'name', label: 'Name', field: 'name', align: 'left', style: 'width: 80px' },
+  { name: 'error', label: 'Error', field: 'error', align: 'left', style: 'width: 80px' },
+  { name: 'args', label: 'Args', field: 'args', align: 'left', style: 'width: 240px' },
+  { name: 'kwargs', label: 'KwArgs', field: 'kwargs', align: 'left', style: 'width: 240px' },
 ];
 
 const simu = useSimulationStore();
